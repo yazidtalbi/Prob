d6a17c98415fa5edfb74883c5e6c57fb42f787eb
Yazid <mti.yazid@gmail.com>
2023-12-25 12:52:07 +0100

    optimisations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import react from "@astrojs/react";
-
 import compress from "astro-compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, react(), compress()],
+  integrations: [tailwind(), astroImageTools, react(), compress(), sitemap()]
 });
